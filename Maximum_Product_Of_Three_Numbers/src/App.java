14b195a26dada71c224e041e438330532509190a
BANGBANGROX <lakshya0809bang@gmail.com>
2022-06-10 23:29:10 +0530

    Added first problem

---
+++ Maximum_Product_Of_Three_Numbers/src/App.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; ++i) {
+                nums[i] = sc.nextInt();
+            }
+
+            Solution solution = new Solution();
+            System.out.println(solution.maximumProduct(nums));
+        }
+
+        sc.close();
+    }
+
+}
